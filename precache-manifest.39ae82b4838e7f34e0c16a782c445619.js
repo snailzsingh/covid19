29a55c2d1a9cb71d988009741163a163a2471c76
Sunil <snailzsingh@gmail.com>
2020-04-29 03:31:00 +0800

    Updates

---
+++ precache-manifest.39ae82b4838e7f34e0c16a782c445619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956d4a1f9a2a7aa10f9da1b35d2f8cac",
+    "url": "/%7Brepo-name%7D/index.html"
+  },
+  {
+    "revision": "717e3eb0926d9b315425",
+    "url": "/%7Brepo-name%7D/static/css/main.04093743.chunk.css"
+  },
+  {
+    "revision": "50346c73733c98d174fd",
+    "url": "/%7Brepo-name%7D/static/js/2.1ed2e62d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/%7Brepo-name%7D/static/js/2.1ed2e62d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717e3eb0926d9b315425",
+    "url": "/%7Brepo-name%7D/static/js/main.8d1fee88.chunk.js"
+  },
+  {
+    "revision": "569e436e9bb9ccd8f46b",
+    "url": "/%7Brepo-name%7D/static/js/runtime-main.d398e510.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/%7Brepo-name%7D/static/media/image.d7265326.png"
+  }
+]);
